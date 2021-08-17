97612650b0f115a337a46ac8d85c736d9ec0a323
gurelbs <guri240@gmail.com>
2021-08-17 17:57:38 +0300

    updated

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ const api = require("../routes/api")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join('/app', '/client/build')));
 app.use('/api',api)
 
 app.get('*', (req, res) => {
-  let file = path.join(__dirname, '/client/build', 'index.html')
+  let file = path.join('/app', '/client/build', 'index.html')
   res.sendFile(file);
 })
 
