3565bbb1e94d1d6e80342aa8d285e00f36b24c3f
gurelbs <guri240@gmail.com>
2021-08-25 01:36:38 +0300

    update

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ app.use((req, res) => {
 })
 
 if (prod) {
-	app.use(express.static('../client/build'))
+	app.use(express.static('client/build'))
 	app.get('/*', (req, res) => {
-		let file = path.join('../client/build', 'index.html')
+		let file = path.join(__dirname,'/client/build', 'index.html')
 		res.sendFile(file)
 	})
 }
