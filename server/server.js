be1df853af84fe8b84c4c5b544cfaf06042200bd
gurelbs <guri240@gmail.com>
2021-08-25 01:21:53 +0300

    add DB

--- server/server.js
+++ server/server.js
@@ -4,28 +4,27 @@ const app = express()
 const path = require('path')
 const api = require('../routes/api')
 require('./../mongoose')
-const mongoose = require('mongoose')
-const {Languages,Language} = require('./../modules/Languages')
-const findOrCreateLanguages = require('./../utils/findOrCreateLanguages')
+const {Language} = require('./../modules/Languages')
 const getLanguages = require('./../controllers/translate/getLanguages')
+const prod = process.env.NODE_ENV === 'production'
 
 app.use(cors())
 app.use(express.json())
 app.use('/api', api)
-let prod = process.env.NODE_ENV === 'production'
+
+app.use((req, res) => {
+	res.status(404)
+	let file = path.join(__dirname, prod ? '/app/client/build' : '../client/public', '404.html')
+	res.sendFile(file)
+})
+
 if (prod) {
 	app.use(express.static('/app/client/build'))
 	app.get('/*', (req, res) => {
 		let file = path.join('/client/build', 'index.html')
-		res.sendFile(file, { root: __dirname })
+		res.sendFile(file)
 	})
 }
-app.use((req, res) => {
-	res.status(404)
-	let file = path.join(__dirname, prod ? '/client/build' : '../client/public', '404.html')
-	res.sendFile(file)
-})
-
 const languageCreator = async () => {
   let count = await Language.countDocuments( (err,res) => res)
   console.log(`Language count: ${count}`)
